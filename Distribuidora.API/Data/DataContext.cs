05853a9732a955dc3593acb3dc77d62f6226e000
LAURA MARIA AGUDELO SANCHEZ <lauraagudelo311517@correo.itm.edu.co>
2023-04-29 18:17:28 -0500

    Commit migracion inicial

---
+++ Distribuidora.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Distribuidora.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Distribuidora.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options): base(options)
+        {
+              
+        }
+
+        public DbSet<Sede> Sedes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Sede>().HasIndex(s => s.Name).IsUnique();
+        }
+
+    }
+}
---
+++ Distribuidora.Shared/Entities/Sede.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distribuidora.Shared.Entities
+{
+    public class Sede
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "Sede")]
+        [MaxLength(100, ErrorMessage ="Cuidado el campo {0} no permite mas de {1} caracteres")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public string Name { get; set; } = null;
+
+    }
+}
