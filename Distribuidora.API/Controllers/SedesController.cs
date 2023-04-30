bfe64e4ffc3cef2ccefca340fb7bdf38219fcd60
LAURA MARIA AGUDELO SANCHEZ <lauraagudelo311517@correo.itm.edu.co>
2023-04-29 19:39:51 -0500

    Commit web y GenericList

---
+++ Distribuidora.API/Controllers/SedesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Distribuidora.API.Data;
+using Distribuidora.Shared.Entities;
+
+
+namespace Distribuidora.API.Controllers
+{
+    [ApiController]
+    [Route("/api/sedes")]
+    public class SedesController: ControllerBase
+    {
+
+        private readonly DataContext _context;
+        private readonly DataContext context;
+
+        public SedesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //get
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Sedes.ToListAsync());
+        }
+
+        //create
+        [HttpPost]
+        public async Task<ActionResult> Post(Sede sede)
+        {
+            _context.Add(sede);
+            await _context.SaveChangesAsync();
+            return Ok(sede);
+        }
+
+    }
+}
---
+++ Distribuidora.WEB/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Distribuidora.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+
+    }
+}
