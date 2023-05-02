09433e4a3799a20312040e65d5cf9ed10464fd89
LAURA MARIA AGUDELO SANCHEZ <lauraagudelo311517@correo.itm.edu.co>
2023-05-02 10:59:21 -0500

    commit img2

--- Distribuidora.API/Program.cs
+++ Distribuidora.API/Program.cs
@@ -46,11 +46,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-//app.UseCors(x => x
-//    .AllowAnyMethod()
-//    .AllowAnyHeader()
-//    .SetIsOriginAllowed(origin => true)
-//    .AllowCredentials());
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
 
 
 app.Run();
